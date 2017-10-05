61f9e5cdc321d3c54b73b374615438b622175146
ilker Yılmaz <ilker@ilkeryilmaz.com>
2017-10-05 14:45:00 +0300

    Fix crawled urls control

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ var baseUrl = url.protocol + "//" + url.hostname;
 
 var requestUrl = function(searchUrl, depth) {
   if (depth <= 3) {
-    request(searchUrl, function(err, resp, body) {
-      $ = cheerio.load(body);
-      pageLinksControl($, depth);
-      console.log("Visiting page: " + searchUrl);
-    });
+    if (crawledUrls.indexOf(searchUrl) === -1) {
+      request(searchUrl, function(err, resp, body) {
+        $ = cheerio.load(body);
+        pageLinksControl($, depth);
+        console.log("Visiting page: " + searchUrl);
+      });
+    }
   }
 }
 
@@ -21,10 +23,8 @@ var pageLinksControl = function($, depth){
   var $relativeLinks = $("a[href^='/']");
   $relativeLinks.each(function() {
     var newUrl = baseUrl + $(this).attr("href");
-    if (crawledUrls.indexOf(searchUrl) === -1) {
-      requestUrl(newUrl, depth+1);
-      crawledUrls.push(newUrl);
-    }
+    requestUrl(newUrl, depth+1);
+    crawledUrls.push(newUrl);
   });
 }
 
