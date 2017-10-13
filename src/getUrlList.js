ce339f2a369be48f262c956ed90a01e24d06b049
ilker Yılmaz <ilker@ilkeryilmaz.com>
2017-10-13 17:34:51 +0300

    Update es6 feature

---
+++ src/getUrlList.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const url = require('url-parse');
+
+const crawledUrls = [];
+
+const requestUrl = (searchUrl, depth) => {
+  const urlParse = new url(searchUrl);
+  const baseUrl = urlParse.protocol + "//" + urlParse.hostname;
+
+  if (depth <= 3) {
+    if (!crawledUrls.includes(searchUrl)) {
+      request(searchUrl, (err, resp, body) => {
+        $ = cheerio.load(body);
+        pageLinksControl($, depth, baseUrl);
+        console.log("Visiting page: " + searchUrl);
+      });
+    }
+  }
+}
+
+const pageLinksControl = ($, depth, baseUrl) => {
+  const $relativeLinks = $("a[href^='/']");
+
+  $relativeLinks.each(($, depth, baseUrl) => {
+    const newUrl = baseUrl + $(this).attr("href");
+    requestUrl(newUrl, depth+1);
+    crawledUrls.push(newUrl);
+  });
+}
+
+
+const init = (searchUrl) => {
+  requestUrl(searchUrl, 1);
+}
+
+
+module.exports = init;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var URL = require('url-parse');
+const getUrlList = require('./getUrlList');
 
-var crawledUrls = [];
-var searchUrl = 'http://www.github.com';
-var url = new URL(searchUrl);
-var baseUrl = url.protocol + "//" + url.hostname;
-
-var requestUrl = function(searchUrl, depth) {
-  if (depth <= 3) {
-    if (crawledUrls.indexOf(searchUrl) === -1) {
-      request(searchUrl, function(err, resp, body) {
-        $ = cheerio.load(body);
-        pageLinksControl($, depth);
-        console.log("Visiting page: " + searchUrl);
-      });
-    }
-  }
-}
-
-var pageLinksControl = function($, depth){
-  var $relativeLinks = $("a[href^='/']");
-  $relativeLinks.each(function() {
-    var newUrl = baseUrl + $(this).attr("href");
-    requestUrl(newUrl, depth+1);
-    crawledUrls.push(newUrl);
-  });
-}
-
-requestUrl(searchUrl, 1);
+getUrlList('http://www.github.com');
